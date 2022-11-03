57439cc82d8b53768144231216a35a7866069806
Diogo Benetti Faé Rodrigues <diogo.fae2001@gmail.com>
2022-11-03 15:22:32 -0500

    Initial commit

---
+++ CoreCV-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreCV-Lab
+//
+//  Created by Diogo Rodrigues on 11/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
