16df2b49fd11aae01b97bc51016a084d6706b942
Diogo Benetti Faé Rodrigues <diogo.fae2001@gmail.com>
2022-11-03 16:32:42 -0500

    Created Modules A and B

---
+++ ImageLab/ViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  ImageLab
+//
+//  Created by Eric Larson
+//  Copyright © Eric Larson. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ModuleB: UIViewController   {
+
+    //MARK: Class Properties
+    var filters : [CIFilter]! = nil
+    var videoManager:VideoAnalgesic! = nil
+    let pinchFilterIndex = 2
+    var detector:CIDetector! = nil
+    let bridge = OpenCVBridge()
+    var flashLight = -1
+    
+    //MARK: Outlets in view
+    @IBOutlet weak var flashSlider: UISlider!
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var camButton: UIButton!
+    @IBOutlet weak var stageLabel: UILabel!
+    
+    //MARK: ViewController Hierarchy
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = nil
+        
+        // setup the OpenCV bridge nose detector, from file
+        self.bridge.loadHaarCascade(withFilename: "nose")
+        
+        self.videoManager = VideoAnalgesic(mainView: self.view)
+        self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.back)
+        
+        // create dictionary for face detection
+        // HINT: you need to manipulate these properties for better face detection efficiency
+        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyLow,CIDetectorTracking:true] as [String : Any]
+        
+        // setup a face detector in swift
+        self.detector = CIDetector(ofType: CIDetectorTypeFace,
+                                  context: self.videoManager.getCIContext(), // perform on the GPU is possible
+            options: (optsDetector as [String : AnyObject]))
+        
+        self.videoManager.setProcessingBlock(newProcessBlock: self.processImageSwift)
+        
+        if !videoManager.isRunning{
+            videoManager.start()
+        }
+    
+    }
+    
+    //MARK: Process image output
+    func processImageSwift(inputImage:CIImage) -> CIImage{
+        var retImage = inputImage
+        
+        self.bridge.setTransforms(self.videoManager.transform)
+        self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
+//        Part 1.2.2
+        var fingerOnScreen = self.bridge.processFinger(Int32(flashLight))
+        
+        retImage = self.bridge.getImage()
+        
+        if fingerOnScreen == true{
+            flashLight += 1
+            self.videoManager.turnOnFlashwithLevel(1.0)
+            DispatchQueue.main.async {
+                self.camButton.isHidden = true
+                self.flashButton.isHidden = true
+            }
+        }
+        else{
+            self.videoManager.turnOffFlash()
+            DispatchQueue.main.async {
+                self.camButton.isHidden = false
+                self.flashButton.isHidden = false
+            }
+        }
+        
+        return retImage
+    }
+    
+    //MARK: Setup Face Detection
+    
+    func getFaces(img:CIImage) -> [CIFaceFeature]{
+        // this ungodly mess makes sure the image is the correct orientation
+        let optsFace = [CIDetectorImageOrientation:self.videoManager.ciOrientation]
+        // get Face Features
+        return self.detector.features(in: img, options: optsFace) as! [CIFaceFeature]
+        
+    }
+    
+    
+    // change the type of processing done in OpenCV
+    @IBAction func swipeRecognized(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+        case .left:
+            self.bridge.processType += 1
+        case .right:
+            self.bridge.processType -= 1
+        default:
+            break
+            
+        }
+        
+        stageLabel.text = "Stage: \(self.bridge.processType)"
+
+    }
+    
+    //MARK: Convenience Methods for UI Flash and Camera Toggle
+    @IBAction func flash(_ sender: AnyObject) {
+        if(self.videoManager.toggleFlash()){
+            self.flashSlider.value = 1.0
+        }
+        else{
+            self.flashSlider.value = 0.0
+        }
+    }
+    
+    @IBAction func switchCamera(_ sender: AnyObject) {
+        self.videoManager.toggleCameraPosition()
+    }
+    
+    @IBAction func setFlashLevel(_ sender: UISlider) {
+        if(sender.value>0.0){
+            let val = self.videoManager.turnOnFlashwithLevel(sender.value)
+            if val {
+                print("Flash return, no errors.")
+            }
+        }
+        else if(sender.value==0.0){
+            self.videoManager.turnOffFlash()
+        }
+    }
+
+   
+}
+
---
+++ ImageLab/ViewControllerMain.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerMain.swift
+//  ImageLab
+//
+//  Created by Diogo Rodrigues on 11/3/22.
+//  Copyright © 2022 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("FIRST")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
