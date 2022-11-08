e41591cf3388beb6fbc75678cba4e861da9c5ce4
Kellen Long <60435086+K3ll3nL@users.noreply.github.com>
2022-11-07 20:04:25 -0600

    Added facial highlighting, smile and blink detection

---
+++ ModuleA.swift
@@ -0,0 +1,173 @@
+ //
+//  ModuleA.swift
+//  ImageLab
+//
+//  Created by Diogo Rodrigues on 11/3/22.
+//  Copyright © 2022 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ModuleA: UIViewController {
+    //MARK: Class Properties
+    var filters : [CIFilter]! = nil
+    var videoManager:VideoAnalgesic! = nil
+    let pinchFilterIndex = 2
+    var detector:CIDetector! = nil
+    let bridge = OpenCVBridge()
+    var flashLight = -1
+    @IBOutlet weak var smiling: NSString!
+    @IBOutlet weak var blinking: NSString!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = nil
+        
+        // setup the OpenCV bridge nose detector, from file
+        self.bridge.loadHaarCascade(withFilename: "nose")
+        
+        self.videoManager = VideoAnalgesic(mainView: self.view)
+        self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.front)
+        
+        // create dictionary for face detection
+        // HINT: you need to manipulate these properties for better face detection efficiency
+        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyLow,CIDetectorTracking:true] as [String : Any]
+        
+        // setup a face detector in swift
+        self.detector = CIDetector(ofType: CIDetectorTypeFace,
+                                  context: self.videoManager.getCIContext(), // perform on the GPU is possible
+            options: (optsDetector as [String : AnyObject]))
+        
+        self.videoManager.setProcessingBlock(newProcessBlock: self.processImageSwift)
+        
+        if !videoManager.isRunning{
+            videoManager.start()
+        }
+    }
+    
+    //MARK: Process image output
+    func processImageSwift(inputImage:CIImage) -> CIImage{
+        
+        // detect faces
+        let f = getFaces(img: inputImage)
+        
+        // if no faces, just return original image
+        if f.count == 0 { return inputImage }
+        
+        var retImage = inputImage
+        
+        smiling = hasASmile(faces: f) ? "True" : "False"
+        blinking = isBlinking(faces:f) ? "True" : "False"
+        
+        //-------------------Example 1----------------------------------
+        // if you just want to process on separate queue use this code
+        // this is a NON BLOCKING CALL, but any changes to the image in OpenCV cannot be displayed real time
+        /*
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) { () -> Void in
+            self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
+            self.bridge.processImage()
+        }
+         */
+        
+        //-------------------Example 2----------------------------------
+        // use this code if you are using OpenCV and want to overwrite the displayed image via OpenCV
+        // this is a BLOCKING CALL
+        /*
+        self.bridge.setTransforms(self.videoManager.transform)
+        self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
+        self.bridge.processImage()
+        retImage = self.bridge.getImage()
+         */
+        
+        //-------------------Example 3----------------------------------
+        //You can also send in the bounds of the face to ONLY process the face in OpenCV
+        // or any bounds to only process a certain bounding region in OpenCV
+        self.bridge.setTransforms(self.videoManager.transform)
+        self.bridge.setImage(retImage,
+                             withBounds: f[0].bounds, // the first face bounds
+                             andContext: self.videoManager.getCIContext())
+        
+        self.bridge.processImage()
+        retImage = self.bridge.getImageComposite() // get back opencv processed part of the image (overlayed on original)
+        
+        return retImage
+    }
+    
+    //MARK: Setup Face Detection
+    
+    func getFaces(img:CIImage) -> [CIFaceFeature]{
+        // this ungodly mess makes sure the image is the correct orientation
+        let optsFace = [CIDetectorImageOrientation:self.videoManager.ciOrientation,CIDetectorSmile:true] as [String : Any]
+        // get Face Features
+        return self.detector.features(in: img, options: optsFace) as! [CIFaceFeature]
+        
+    }
+
+    func hasASmile(faces:[CIFaceFeature]) -> Bool {
+        for face in faces {
+            if(!face.hasSmile){
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isBlinking(faces:[CIFaceFeature]) -> Bool {
+        for face in faces {
+            if(face.leftEyeClosed && face.rightEyeClosed){
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+    // change the type of processing done in OpenCV
+    @IBAction func swipeRecognized(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+        case .left:
+            self.bridge.processType += 1
+        case .right:
+            self.bridge.processType -= 1
+        default:
+            break
+            
+        }
+        
+//        stageLabel.text = "Stage: \(self.bridge.processType)"
+
+    }
+    
+    //MARK: Convenience Methods for UI Flash and Camera Toggle
+//    @IBAction func flash(_ sender: AnyObject) {
+//        if(self.videoManager.toggleFlash()){
+//            self.flashSlider.value = 1.0
+//        }
+//        else{
+//            self.flashSlider.value = 0.0
+//        }
+//    }
+    
+    @IBAction func switchCamera(_ sender: AnyObject) {
+        self.videoManager.toggleCameraPosition()
+    }
+    
+    @IBAction func setFlashLevel(_ sender: UISlider) {
+        if(sender.value>0.0){
+            let val = self.videoManager.turnOnFlashwithLevel(sender.value)
+            if val {
+                print("Flash return, no errors.")
+            }
+        }
+        else if(sender.value==0.0){
+            self.videoManager.turnOffFlash()
+        }
+    }
+    
+    
+    
+
+   
+}
+
